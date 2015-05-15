1741daa887b05c414250d8d2d0e126cd3a63d807
Aaron Vinson <avinson.public@gmail.com>
2015-05-14 17:01:59 -0700

    use sorted pairs function

--- heka-testing/json.lua
+++ heka-testing/json.lua
@@ -1,23 +1,46 @@
 local cjson = require "cjson"
  
 -- opens a file in read
-file = io.open("blob.json", "r")
+local file = io.open("blob.json", "r")
  
 -- sets the default input file as test.lua
 io.input(file)
  
 -- read the file
-local s = io.read('*all')
+local s = io.read('*a')
  
 -- closes the open file
 io.close(file)
  
 local json = cjson.decode(s)
  
+function spairs(t, order)
+    -- collect the keys
+    local keys = {}
+    for k in pairs(t) do keys[#keys+1] = k end
+
+    -- if order function given, sort by it by passing the table and keys a, b,
+    -- otherwise just sort the keys 
+    if order then
+        table.sort(keys, function(a,b) return order(t, a, b) end)
+    else
+        table.sort(keys)
+    end
+
+    -- return the iterator function
+    local i = 0
+    return function()
+        i = i + 1
+        if keys[i] then
+            return keys[i], t[keys[i]]
+        end
+    end
+end
+
 function expand(json)
-  for k, v in pairs(json) do
+  for k, v in spairs(json) do
     if type(v) == "table" then
-      for l, w in pairs(expand(v)) do
+      for l, w in spairs(expand(v)) do
         json[k .. '.' .. l] = w
       end
       json[k] = nil
